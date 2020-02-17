7960f95557c808c36f17cffda27d237b6544f12c
Laurent Binder <laurentbinder@gmail.com>
2020-02-17 12:22:15 +0100

    url card category ''

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,15 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('sweetalert2/dist/sweetalert2.min')
 
 import 'bootstrap';
 
-
+document.addEventListener('turbolinks:load', () => {
+  initSweetalert();
+  initTyped();
+  // etc...
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
