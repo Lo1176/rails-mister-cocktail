f64f7af0ff9c72d4489d95e7e929b283bcd02776
Laurent Binder <laurentbinder@gmail.com>
2020-02-17 12:36:55 +0100

    change cloudinary in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,8 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -54,7 +55,6 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # active storage configuration @lewagon
-  config.active_storage.service = :cloudinary
 
 
   # Raises error for missing translations.
