b8806ca160d6d9a23cf0f0f18c32f553beb7af96
Laurent Binder <laurentbinder@gmail.com>
2020-04-05 17:57:05 +0200

    set_cocktail before_action

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,18 +1,18 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, except: :destroy
   def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
+    # @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
-    @cocktail = Cocktail.find(params[:cocktail_id])
+    # @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
-    # @ingredient = Ingredient.find(params[:dose][:ingredient_id])
-    # @dose.ingredient = @ingredient
     if @dose.save
       redirect_to cocktail_path(@dose.cocktail)
     else
+      # render 'new'
       render 'cocktails/show'
     end
   end
@@ -24,6 +24,10 @@ class DosesController < ApplicationController
   end
 
   private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
