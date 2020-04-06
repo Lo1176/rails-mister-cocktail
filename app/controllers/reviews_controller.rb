1fb0108ed1276064525230568726217d23461e8e
Laurent Binder <laurentbinder@gmail.com>
2020-04-06 15:02:49 +0200

    deleted all spec and try to add reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,27 @@
 class ReviewsController < ApplicationController
+  before_action :find_cocktail
+
+  def new
+    @review = Review.new
+  end
+
   def create
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @review = Review.new(review_params)
     @review.cocktail = @cocktail
     if @review.save
-      redirect_to cocktail_path(@cocktail)
+      redirect_to cocktails_path(@cocktail)
     else
       @dose = Dose.new
       render 'cocktails/show'
+      # render :new
     end
   end
 
   private
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
 
   def review_params
     params.require(:review).permit(:content, :rating)
