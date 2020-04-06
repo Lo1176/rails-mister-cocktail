a96aef4041968b77405a81779f02828ef9f5102c
Laurent Binder <56685868+Lo1176@users.noreply.github.com>
2020-04-06 15:26:55 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: 'cocktails#index'
   resources :cocktails, only: %i[index show new create] do
     resources :doses, only: [:create]
-    # resources :reviews, only: [:create]
+    resources :reviews, only: [:create]
   end
   resources :doses, only: [:destroy]
 end
